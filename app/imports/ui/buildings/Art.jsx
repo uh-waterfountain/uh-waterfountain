502dcf5df6659688c33abd843822c7629900d024
jakecastillo <jakecast@hawaii.edu>
2020-05-12 21:46:16 -1000

    current state

--- app/imports/ui/buildings/Art.jsx
+++ app/imports/ui/buildings/Art.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Card, Container, Header, Loader } from 'semantic-ui-react';
+import { Container, Header, Loader, Image, Grid, Rating, Card, Item } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import FountainItem from '/imports/ui/components/FountainItem'
+import { Ratings } from '../../api/rating/Rating';
 import { Fountains } from '../../api/fountain/Fountain';
 import Fountain from '../components/Fountain';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class Art extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePage: 1,
+    };
+  }
+
+  handleChange = (e, data) => {
+    this.setState({ activePage: data.activePage });
+  };
+
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
@@ -20,6 +34,12 @@ class Art extends React.Component {
           <Header as="h2" textAlign="center" inverted>Art</Header>
           <Card.Group>
             {this.props.fountains.map((fountain, index) => <Fountain key={index} fountain={fountain}/>)}
+            <Item.Group divided>
+              {this.props.fountains.slice((this.state.activePage - 1) * 5,
+                  this.state.activePage * 5).map((fountain, index) => <FountainItem key={index} Fountains={Fountains}
+                                                                                    fountain={fountain}
+                                                                                    Ratings={Ratings} rating={this.props.ratings}/>)}
+            </Item.Group>
           </Card.Group>
         </Container>
     );
@@ -29,6 +49,7 @@ class Art extends React.Component {
 /** Require an array of Stuff documents in the props. */
 Art.propTypes = {
   fountains: PropTypes.array.isRequired,
+  ratings: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
@@ -36,8 +57,10 @@ Art.propTypes = {
 export default withTracker(() => {
   // Get access to Stuff documents.
   const subscription = Meteor.subscribe('Fountains');
+  const subscription2 = Meteor.subscribe('Ratings');
   return {
-    fountains: Fountains.find({}).fetch(),
-    ready: subscription.ready(),
+    fountain: Fountains.find({}).fetch(),
+    ratings: Ratings.find({}).fetch(),
+    ready: (subscription.ready() && subscription2.ready()),
   };
 })(Art);
