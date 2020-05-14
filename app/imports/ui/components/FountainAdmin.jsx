45bfe327bd48303932e41e1bf7a7cf494548ebec
Sean Sumida <sumidase@hawaii.edu>
2020-05-13 18:22:03 -1000

    unfinished

---
+++ app/imports/ui/components/FountainAdmin.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Card, CardContent } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class FountainAdmin extends React.Component {
+  render() {
+    return (
+        <Card centered>
+          <Card.Content>
+            <Card.Header>{this.props.fountain.name}</Card.Header>
+            <CardContent>hello</CardContent>
+            <img
+                src={this.props.fountain.image}
+                height={150}
+                width={262}
+            />
+          </Card.Content>
+        </Card>
+    );
+  }
+}
+
+/** Require a document to be passed to this component. */
+FountainAdmin.propTypes = {
+  fountain: PropTypes.object.isRequired,
+};
+
+/** Wrap this component in withRouter since we use the <Link> React Router element. */
+export default withRouter(FountainAdmin);
