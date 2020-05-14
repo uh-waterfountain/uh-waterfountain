a7bbde89bc1f7b4937f811825d58ab63c5bef7fb
gbaik00 <60208409+gbaik00@users.noreply.github.com>
2020-05-13 18:46:28 -1000

    Added ART layout to all buildings

--- app/imports/ui/buildings/Qlc.jsx
+++ app/imports/ui/buildings/Qlc.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Card, Container, Header, Loader } from 'semantic-ui-react';
+import { Container, Header, Loader, Card } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import FountainItem from '/imports/ui/components/FountainItem';
+import { Ratings } from '../../api/rating/Rating';
 import { Fountains } from '../../api/fountain/Fountain';
-import Fountain from '../components/Fountain';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class Qlc extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePage: 1,
+    };
+  }
+
+  handleChange = (e, data) => {
+    this.setState({ activePage: data.activePage });
+  };
+
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
@@ -16,11 +29,14 @@ class Qlc extends React.Component {
   /** Render the page once subscriptions have been received. */
   renderPage() {
     return (
-        <Container >
-          <Header as="h2" textAlign="center" inverted>Qlc</Header>
-          <Card.Group>
-            {this.props.fountains.map((fountain, index) => <Fountain key={index} fountain={fountain}/>)}
-          </Card.Group>
+        <Container>
+          <Header as="h2" textAlign="center" inverted>Queen Liliuokalani Center</Header>
+          <Card.Content>
+            {this.props.fountains.map((fountain, index) => <FountainItem key={index} Fountains={Fountains}
+                                                                         fountain={fountain}
+                                                                         Ratings={Ratings}
+                                                                         rating={this.props.ratings}/>)}
+          </Card.Content>
         </Container>
     );
   }
@@ -29,6 +45,7 @@ class Qlc extends React.Component {
 /** Require an array of Stuff documents in the props. */
 Qlc.propTypes = {
   fountains: PropTypes.array.isRequired,
+  ratings: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
@@ -36,8 +53,10 @@ Qlc.propTypes = {
 export default withTracker(() => {
   // Get access to Stuff documents.
   const subscription = Meteor.subscribe('Fountains');
+  const subscription2 = Meteor.subscribe('Ratings');
   return {
-    fountains: Fountains.find({ location: 'QLC' }).fetch(),
-    ready: subscription.ready(),
+    fountains: Fountains.find({}).fetch(),
+    ratings: Ratings.find({}).fetch(),
+    ready: (subscription.ready() && subscription2.ready()),
   };
 })(Qlc);
